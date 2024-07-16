a1a334f5cb99f6fca2df915ac7976590ef40b6fd
carolaine-vasconcelos <vasconcelos.carol.246@gmail.com>
2024-07-16 17:28:11 -0300

    commit full course

---
+++ estudos/exercicios/ex014.js
@@ -0,0 +1,13 @@
+var c = 1
+
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//Faça um dos dois testes
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
---
+++ estudos/exercicios/ex024.js
@@ -0,0 +1,34 @@
+// DECLARAR UMA VARIÁVEL
+var nome = 9;
+let fruta = "maça";
+
+// TIPOS DE VARIAVEIS
+// STRING
+var nome = "Maria";
+
+// INTEGERS
+var idade = 18;
+
+// FLOAT
+var contaBancaria = 80.05;
+
+// Boleano tem dois valores: verdadeiro ou falso
+var MariaCabeloPreto = false;
+
+// Arrays: contem diversos valores
+var frutas = {
+    "Maça", "Banana", "Laranja"
+};
+ 
+// Para acessar um item especifico dentro de um array
+frutas[0]
+
+// Objetos
+var xicara = {
+    cor: "branco",
+    material: "porcelana",
+    peso: 100
+};
+
+// Para acessar a propriedade de um objeto é so colocar um ponto
+xicara.cor
